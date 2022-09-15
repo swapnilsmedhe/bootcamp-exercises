9f5fd0f6cc9f15b622c6bc99dffb1e568bc772d1
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-09-15 13:52:47 +0530

    Added CM and MM units for length

--- src/com/tw/step8/assignment3/Unit.java
+++ src/com/tw/step8/assignment3/Unit.java
@@ -2,7 +2,9 @@ package com.tw.step8.assignment3;
 
 public enum Unit {
     INCH(2.5),
-    FEET(30);
+    FEET(30),
+    CM(1),
+    MM(0.1);
 
     public final double centimeterConstant;
 
--- test/com/tw/step8/assignment3/LengthTest.java
+++ test/com/tw/step8/assignment3/LengthTest.java
@@ -24,4 +24,40 @@ class LengthTest {
         assertTrue(twelveInches.isEqual(oneFeet));
         assertFalse(fiveInches.isEqual(threeFeet));
     }
+
+    @Test
+    void compareInchAndCentimeterForEquality() {
+        Length twoInches = new Length(2, Unit.INCH);
+        Length fiveCentimeters = new Length(5, Unit.CM);
+
+        Length oneInch = new Length(1, Unit.INCH);
+        Length twoCentimeters = new Length(2, Unit.CM);
+
+        assertTrue(twoInches.isEqual(fiveCentimeters));
+        assertFalse(oneInch.isEqual(twoCentimeters));
+    }
+
+    @Test
+    void compareCentimeterAndMillimeterForEquality() {
+        Length oneCentimeter = new Length(1, Unit.CM);
+        Length tenMillimeters = new Length(10, Unit.MM);
+
+        Length fiveCentimeter = new Length(5, Unit.CM);
+        Length fifteenMillimeters = new Length(15, Unit.MM);
+
+        assertTrue(oneCentimeter.isEqual(tenMillimeters));
+        assertFalse(fiveCentimeter.isEqual(fifteenMillimeters));
+    }
+
+    @Test
+    void compareMillimeterAndInchForEquality() {
+        Length hundredMillimeters = new Length(100, Unit.MM);
+        Length fourInches = new Length(4, Unit.INCH);
+
+        Length twoHundredMillimeters = new Length(200, Unit.MM);
+        Length fiveInches = new Length(5, Unit.INCH);
+
+        assertTrue(hundredMillimeters.isEqual(fourInches));
+        assertFalse(twoHundredMillimeters.isEqual(fiveInches));
+    }
 }
